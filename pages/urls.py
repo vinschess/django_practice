2051d3d845b1d8d327b70c850dcb920a32bc52ee
Vinay Gupta <vinay.gupta@treebohotels.com>
2019-01-23 12:08:14 +0530

    Add first model - Post

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from pages.views import AboutPageView
+# from pages.views import AboutPageView
 from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
+    # path('about/', AboutPageView.as_view(), name='about'),
 ]
